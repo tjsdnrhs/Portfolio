ce5659ae4fe091552200404446f5f032b8daeb43
tjsdnrhs <tjsdnrhs@naver.com>
2023-11-30 17:50:55 +0900

    231130

--- script.js
+++ script.js
@@ -16,7 +16,6 @@ const s_Right = document.querySelector('.slide-btn.right');
 const Bg = document.querySelector('#sec3');
 const s_Slider = document.querySelectorAll('.sec3-slider div');
 const a_Box = document.querySelectorAll('.a-box a')
-console.log(a_Box.length);
 const s_reset = () => {
   s_Slider.forEach((elem, idx) => {
     s_Slider[idx].classList.remove('on');
@@ -80,3 +79,75 @@ s_Left.addEventListener('click', prev);
 Nav_btn.addEventListener('click', () => {
   Body.classList.toggle('nav-active');
 });
+
+
+// section5 slider
+// const sec4-s_Icons = document.querySelectorAll('.sec4-slide-icons li');
+// const sec4-s_Left = document.querySelector('.sec4-slide-btn.left');
+// const sec4-s_Right = document.querySelector('.sec4-slide-btn.right');
+
+// const sec4-s_Slider = document.querySelectorAll('.sec4-inner');
+// const sec4-a_Box = document.querySelectorAll('.a-box a')
+// const sec4-s_reset = () => {
+//   sec4-s_Slider.forEach((elem, idx) => {
+//     sec4-s_Slider[idx].classList.remove('on');
+//     sec4-s_Icons[idx].classList.remove('active');
+//     sec4-a_Box[idx].classList.remove('active')
+//   });
+// };
+
+// s_Icons.forEach((li) => {
+//   li.addEventListener('click', (e) => {
+//     let target = e.target.dataset.index;
+//     console.log(target);
+//     s_reset();
+//     if (li.tagName === 'LI') {
+//       for (let i = 0; i < s_Icons.length; i++) {
+//         if (target == i) {
+//           sec4-s_Slider[i].classList.add('on');
+//           sec4-s_Icons[i].classList.add('active');
+//           sec4-a_Box[i].classList.add('active')
+
+//         }
+//       }
+//     }
+//   });
+// });
+
+// const sec4-next = (e) => {
+//   e.preventDefault();
+//   let crtSlide = document.querySelector('.sec4-inner.on');
+//   let i = crtSlide.dataset.index;
+//   s_reset();
+//   i++;
+//   if (i >= s_Slider.length) {
+//     i = 0;
+//   }
+//   sec4-s_Slider[i].classList.add('on');
+//   sec4-s_Icons[i].classList.add('active');
+//   sec4-a_Box[i].classList.add('active');
+
+// };
+
+// s_Right.addEventListener('click', next);
+
+// const sec4-prev = (e) => {
+//   e.preventDefault();
+//   let crtSlide = document.querySelector('.sec4-inner.on');
+//   let i = crtSlide.dataset.index;
+//   s_reset();
+//   i--;
+//   if (i < 0) {
+//     i = s_Slider.length - 1;
+//   }
+//   sec4-s_Slider[i].classList.add('on');
+//   sec4-s_Icons[i].classList.add('active');
+//   sec4-a_Box[i].classList.add('active');
+
+// };
+
+// s_Left.addEventListener('click', prev);
+
+// Nav_btn.addEventListener('click', () => {
+//   Body.classList.toggle('nav-active');
+// });
